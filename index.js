b1fbd63672b14f81d68dd8c5c0cc8bb53512c32e
Phong <papker2020@gmail.com>
2022-10-23 15:39:56 +0700

    bai 2 v2

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 //INPUT: nhập vào đối tượng người dùng
 //PROCESS:(pseudo code)
-    // kiểm ra ai đang dùng
+    // kiểm ra ai đang dùng bằng cách
+    // if(user === B){
+    //     console.log("Con chào Bố !!!");
+    // }
+    // else if(user === M){
+    //     console.log("Con chào Mẹ !!!");
+    // }
+    // else if(user === A){
+    //     console.log("Em chào Anh !!!");
+    // }
+    // else{
+    //     console.log(" chào Em !!!");
+    // }
 //OUTput: xuất câu xin chào
 var B = "Bố";
 var M = "Mẹ";
